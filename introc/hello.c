bfbc2f62531fc5f51cc669a51640142da1dc36c9
Devin J. Pohly <djpohly@gmail.com>
2025-01-13 15:26:14 -0600

    hello example

---
+++ introc/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// public static void main(String[] args)
+int main(void) {
+	char str[10];
+	str[0] = 'T';
+	str[1] = 'r';
+	str[2] = 'e';
+
+	printf("Hello, %s!\n", str);
+	return 0;
+}
